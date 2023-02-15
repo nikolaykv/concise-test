056f31307610360c550899f656eb37db18fe077f
Kvasov Nikolai <kvasov1992@inbox.ru>
2023-02-15 12:56:53 +0400

    Модели и связи

---
+++ backend/models/Product.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "product".
+ *
+ * @property int $id
+ * @property string|null $image
+ * @property int $is_deleted
+ */
+class Product extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'product';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['is_deleted'], 'integer'],
+            [['image'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'image' => 'Image',
+            'is_deleted' => 'Is Deleted',
+        ];
+    }
+}
---
+++ backend/models/StoreProduct.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "store_product".
+ *
+ * @property int $id
+ * @property int $product_id
+ * @property string|null $product_image
+ *
+ * @property Product $product
+ */
+class StoreProduct extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'store_product';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['product_id'], 'required'],
+            [['product_id'], 'integer'],
+            [['product_image'], 'string', 'max' => 255],
+            [['product_id'], 'exist', 'skipOnError' => true, 'targetClass' => Product::class, 'targetAttribute' => ['product_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'product_id' => 'Product ID',
+            'product_image' => 'Product Image',
+        ];
+    }
+
+    /**
+     * Gets query for [[Product]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getProduct()
+    {
+        return $this->hasOne(Product::class, ['id' => 'product_id']);
+    }
+}
